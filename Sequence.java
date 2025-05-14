0b23e8c940958c4ec1ec92b76beb19756eeb9013
karthikvarigonda <kvarigon@purdue.edu>
2025-05-14 11:45:53 -0400

    m

--- Sequence.java
+++ Sequence.java
@@ -1,2 +1,29 @@
 public class Sequence {
+    private String id;
+    private String nucleotideSeq;
+    private String proteinSeq;
+
+    public Sequence(String id, String nucleotideSeq) {
+        this.id = id;
+        this.nucleotideSeq = nucleotideSeq;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getNucleotideSeq() {
+        return nucleotideSeq;
+    }
+    public String getProteinSeq() {
+        return proteinSeq;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setNucleotideSeq(String nucleotideSeq) {
+        this.nucleotideSeq = nucleotideSeq;
+    }
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nNucleotide: " + nucleotideSeq;
+    }
 }
